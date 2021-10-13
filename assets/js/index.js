e49ec93cf8ac639bb8f577d4adc68434b83d3e40
Isha Sidhu <ishasimranxo@gmail.com>
2021-10-13 15:04:39 -0600

    changes made

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
+'use strict';
+
 // Variables
 
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".closeIcon");
 
 // Function Statement
 
