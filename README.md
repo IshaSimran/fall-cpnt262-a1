a9ef6eaeb09f86913af6bdd9bd534dd9741651b5
Isha Sidhu <ishasimranxo@gmail.com>
2021-10-13 17:25:51 -0600

    changes made

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Fall-CPNT262-A1 : classList Toggle
 ## By Isha Simran
 
-- [GitHub Repo]()
+- [GitHub Repo](https://github.com/IshaSimran/fall-cpnt262-a1)
 
-- [GH Pages Site]()
+- [GH Pages Site](https://ishasimran.github.io/fall-cpnt262-a1/)
 
 
 ## Attributions
 
-### Icons
-
 ### Fonts
+
+- [Mukta Font](https://fonts.google.com/specimen/Mukta) designed by Ek Type
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,32 @@
 
 // Variables
 
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector(".hamburger");
-const menuIcon = document.querySelector(".menuIcon");
-const closeIcon = document.querySelector(".closeIcon");
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const menuIcon = document.querySelector('.menuIcon');
+const closeIcon = document.querySelector('.closeIcon');
 
 // Function Statement
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
   }
 }
 
 // Event Listeners
 
-hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
 menuItems.forEach(
   function (menuItem) {
-    menuItem.addEventListener("click", toggleMenu);
+    menuItem.addEventListener('click', toggleMenu);
   }
 )
